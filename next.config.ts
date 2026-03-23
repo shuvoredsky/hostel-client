772aaa7be638b6f1bb5be322eda0779fad1b531a
shuvo chakrabrati <kumarshuvo265@gmail.com>
2026-03-23 17:08:02 +0600

    update my next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
